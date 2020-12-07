4b335573c1feeb61d87e39a5beac3d9c063a69cb
kurema <kurema_makoto_software@yahoo.co.jp>
2020-12-08 02:41:01 +0900

    remember bad code

---
+++ qiitaSourceGenerator/TestConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sber = new QiitaSourceGenerator.Helper.StringBuilderChains.StringBuilderChainsEmpty();
+            var sbe2 = sber + "hello";
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
